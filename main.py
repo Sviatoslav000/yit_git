36edd6d8ffc574f2c02c5c700e2b4b6418fd34a2
Sviatoslav000 <svyatyablonsky@gmail.com>
2025-03-08 00:21:19 +0200

    Test workflow

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sqlite3
 from flask import Flask, render_template, jsonify
 
 app = Flask(__name__, template_folder="templates")
-
+print("The site is up and running")
 # Функція для отримання останнього запису з БД
 def get_last_record():
     connection = sqlite3.connect('generator.db')
@@ -15,7 +15,7 @@ def get_last_record():
         generators.fuleusage, 
         generators.fulelevel, 
         generators.emmision, 
-        generators.timestamp, 
+        generators.timestamp,  
         names.name AS generator_name
         FROM 
             generators
